4c2f06d394181d86ffdac07e6c8045080ee8c84d
Christopher Baum <christopherabaum@gmail.com>
2023-05-12 10:48:28 -0500

    Import router features into App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
 	return (
-		<>
-			<h1>To-Do Guru</h1>
-			<input
-			type="text"
-			placeholder="Name of Household"
-			/>
-			<button>Submit</button>
-		</>
+		<main>
+			<Switch>
+				<Route exact path="/">
+					<h1>To-Do Guru</h1>
+					<input
+					type="text"
+					placeholder="Name of Household"
+					/>
+					<button>Submit</button>
+				</Route>
+
+				<Route path="*">
+					<Redirect from="*" to="/" />
+				</Route>
+			</Switch>
+		</main>
 	);
 }
 
