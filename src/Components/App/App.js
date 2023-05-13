48872cf1dff77e7c33a9df0778dc0f7203d51ace
Christopher Baum <christopherabaum@gmail.com>
2023-05-13 14:23:47 -0500

    Add aside to ChoreForm with users chores

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,7 +14,7 @@ const App = ()  => {
 					<Login />
 					<input
 					type="text"
-					placeholder="Name of Household"
+					placeholder="Username/Email"
 					/>
 				</Route>
 
--- src/Components/ChoreForm/ChoreForm.js
+++ src/Components/ChoreForm/ChoreForm.js
@@ -1,11 +1,26 @@
 import './ChoreForm.css';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 function ChoreForm() {
+
+    const [chores, setChores] = useState([
+        'Wash Dishes',
+        'Laundry',
+        'Sweep Floor'
+    ])
+
+    const choreItems = chores.map((chore, index) => <li key={index}>{chore}</li>)
+
     return (
         <>
             <h1>Edit your Chores!</h1>
 
+            <aside className='chore-list'>
+                <ol>
+                    {choreItems}
+                </ol>
+            </aside>
 
             <NavLink to="/dashboard">
 				<button>View Schedule</button>
