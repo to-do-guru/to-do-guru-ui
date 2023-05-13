26a9b54f60fe852a69092b2249970b129c58c336
Travis Parks <travisparks0788@gmail.com>
2023-05-13 11:45:45 -0400

    Add state and buttons to dashboard component

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,15 +1,37 @@
 import './Dashboard.css';
 import ChoreCard from '../ChoreCard/ChoreCard';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { useState } from 'react';
 
-function Dashboard() {
+const Dashboard = () => {
+
+const [dayOfWeek, setDayOfWeek] = useState("")
+
     return (
         <>
             <h1>I AM A Dashboard</h1>
-            <ChoreCard />
+
+            <button>Monday</button>
+            <button>Tuesday</button>
+            <button>Wedesday</button>
+            <button>Thursday</button>
+            <button>Friday</button>
+            <button>Saturday</button>
+            <button>Sunday</button>
+
+            <div>
+                <ChoreCard />
+            </div>
             <NavLink to="/">
 				<button>Log Out</button>
 			</NavLink>
+            <button>Get me a new schedule</button>
+            <NavLink to="/choreform">
+                <button>Edit Chore List</button>
+            </NavLink>
+            <NavLink to="/houseform">
+                <button>Edit Household</button>
+            </NavLink>
         </>
     );
 }
