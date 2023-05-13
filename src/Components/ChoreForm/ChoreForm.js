9ddaf46795f56866fd0a0ca35bec6df3f69f0611
Christopher Baum <christopherabaum@gmail.com>
2023-05-13 14:41:39 -0500

    Install npm package for multi select input and add to ChoreForm

--- src/Components/ChoreForm/ChoreForm.js
+++ src/Components/ChoreForm/ChoreForm.js
@@ -1,6 +1,7 @@
 import './ChoreForm.css';
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import Select from 'react-select';
 
 function ChoreForm() {
 
@@ -10,18 +11,44 @@ function ChoreForm() {
         'Sweep Floor'
     ])
 
+    const daysOfTheWeek = [
+        { value: 'Monday', label: 'Monday' },
+        { value: 'Tuesday', label: 'Tuesday' },
+        { value: 'Wednesday', label: 'Wednesday' },
+        { value: 'Thursday', label: 'Thursday' },
+        { value: 'Friday', label: 'Friday' },
+        { value: 'Saturday', label: 'Saturday' },
+        { value: 'Sunday', label: 'Sunday' }
+    ]
+
     const choreItems = chores.map((chore, index) => <li key={index}>{chore}</li>)
 
     return (
         <>
             <h1>Edit your Chores!</h1>
-
-            <aside className='chore-list'>
-                <ol>
-                    {choreItems}
-                </ol>
-            </aside>
-
+            <div className='chore-form-container'>
+                <form className='chore-form'>
+                    <label>
+                        Chore Name:
+                        <input 
+                            type='text'
+                        />
+                    </label>
+                    <label>
+                        Days of the week for this chore:
+                        <Select
+                            isMulti 
+                            options={daysOfTheWeek}
+                        />
+                    </label>
+                </form>
+                <aside className='chore-list'>
+                    <h2>Your Chores:</h2>
+                    <ol>
+                        {choreItems}
+                    </ol>
+                </aside>
+            </div>
             <NavLink to="/dashboard">
 				<button>View Schedule</button>
 			</NavLink>
