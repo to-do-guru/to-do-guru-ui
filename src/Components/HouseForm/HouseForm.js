65c81afa15c5a7d497d3a0b1df28fff0babd0792
Christopher Baum <christopherabaum@gmail.com>
2023-05-13 13:28:52 -0500

    Add increment input to allow for adding household members names

--- src/Components/HouseForm/HouseForm.js
+++ src/Components/HouseForm/HouseForm.js
@@ -6,18 +6,47 @@ function HouseForm() {
 
     const [input, setInput] = useState({
         houseName: '',
-        members: []
-    })
+        membersNames: [],
+        membersInputs: []
+    });
+
+    const changeName = (name) => {
+        setInput({...input, houseName: name});
+    }
+
+    const createMemberInputs = input.membersInputs.map((member, index) => 
+        <input 
+            key={index}
+            type='text'
+            name={`memberName${index}`}
+            placeholder='Name of Chore-Doer'
+        />
+    )
 
     return (
         <>
             <h1>Edit your Household!</h1>
-            <input 
-                type='text'
-                name='householdName'
-                placeholder='Name Your Household'
-                value={input.houseName}
-            />
+            <form>
+                <input 
+                    type='text'
+                    name='householdName'
+                    placeholder='Name Your Household'
+                    value={input.houseName}
+                    onChange={(e) => changeName(e.target.value)}
+                />
+                <label>
+                    How many chore-doers are there?
+                    <input
+                        type='number'
+                        step='1'
+                        min='1'
+                        max='10'
+                        value={input.membersInputs.length}
+                        onChange={() => setInput({...input, membersInputs: [...input.membersInputs, 'newInput']})}
+                    />
+                </label>
+                {createMemberInputs}
+            </form>
             <NavLink to="/dashboard">
 				<button>See Schedule</button>
 			</NavLink>
