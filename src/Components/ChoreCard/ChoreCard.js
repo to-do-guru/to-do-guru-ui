70bc6ab37743bf43b5da8535b61992b5bfbae080
Travis Parks <travisparks0788@gmail.com>
2023-05-13 13:25:35 -0400

    Added border and rearranged chore card layout

--- src/Components/ChoreCard/ChoreCard.js
+++ src/Components/ChoreCard/ChoreCard.js
@@ -3,10 +3,9 @@ import './ChoreCard.css';
 const ChoreCard = ({member, chore, duration}) => {
     return (
         <div className='chore-card'>
-            <h1>I AM A ChoreCard</h1>
-            <h2>{member}</h2>
-            <p>{chore}</p>
-            <p>{duration}</p>
+            <h3>{member}'s Chore:</h3>
+            <h2>{chore}</h2>
+            <p>{duration} minutes</p>
         </div>
     );
 }
