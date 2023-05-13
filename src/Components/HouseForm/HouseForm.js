ae80a2d6fb923a5f95c5d9fd9e25445ced02bd5f
Christopher Baum <christopherabaum@gmail.com>
2023-05-13 12:47:45 -0500

    Add state and controlled form input to HouseForm

--- src/Components/HouseForm/HouseForm.js
+++ src/Components/HouseForm/HouseForm.js
@@ -1,10 +1,23 @@
 import './HouseForm.css';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 function HouseForm() {
+
+    const [input, setInput] = useState({
+        houseName: '',
+        members: []
+    })
+
     return (
         <>
             <h1>Edit your Household!</h1>
+            <input 
+                type='text'
+                name='householdName'
+                placeholder='Name Your Household'
+                value={input.houseName}
+            />
             <NavLink to="/dashboard">
 				<button>See Schedule</button>
 			</NavLink>
