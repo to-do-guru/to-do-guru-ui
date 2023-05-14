9d28ae78bf99293ffd06d145a32f336762cfb292
Kara Anderson <kanderson8585@gmail.com>
2023-05-13 19:08:40 -0600

    Style login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,15 +2,19 @@ import './Login.css';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Login() {
-    return (
-        <>
-            <h1>Welcome to To-Do Guru!</h1>
-            <h2>Login to view your chore schedule!</h2>
-            <NavLink to="/dashboard">
-				<button>Login</button>
-			</NavLink>
-        </>
-    );
+  return (
+    <div className='login'>
+      <h1>Welcome to To-Do Guru!</h1>
+      <h2>Login to view your chore schedule!</h2>
+      <NavLink to="/dashboard">
+      <input
+				type="text"
+				placeholder="Username/Email"
+			/>
+      <button className='login-btn'>Login</button>
+      </NavLink>
+    </div>
+  );
 }
 
 export default Login;
