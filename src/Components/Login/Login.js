e0d695a878a1f5cba3e3c3201ae9ecf0c04fb6b0
Kara Anderson <kanderson8585@gmail.com>
2023-05-14 14:55:02 -0600

    Fix login bug

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,11 +6,11 @@ function Login() {
     <div className='login'>
       <h1>Welcome to To-Do Guru!</h1>
       <h2>Login to view your chore schedule!</h2>
-      <NavLink to="/dashboard">
       <input
 				type="text"
 				placeholder="Username/Email"
 			/>
+      <NavLink to="/dashboard">
       <button className='login-btn'>Login</button>
       </NavLink>
     </div>
