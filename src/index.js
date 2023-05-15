d0ea28a94b5300e4a6ce72e48d5f68696e872fe9
Kara Anderson <kanderson8585@gmail.com>
2023-05-15 13:46:17 -0600

    Test API example endpoint

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://411adca7-4489-40e4-9b8b-f7a45bf4102e.mock.pstmn.io/graphql/",
+  uri: "https://411adca7-4489-40e4-9b8b-f7a45bf4102e.mock.pstmn.io",
   cache: new InMemoryCache()
 });
 
