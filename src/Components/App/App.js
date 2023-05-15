00936b6c61bda5e3dfe0e6f010b214c9ebdd978a
Kara Anderson <kanderson8585@gmail.com>
2023-05-15 13:28:46 -0600

    Install GraphQL

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,11 +4,23 @@ import Login from '../Login/Login';
 import HouseForm from '../HouseForm/HouseForm';
 import ChoreForm from '../ChoreForm/ChoreForm';
 import Dashboard from '../Dashboard/Dashboard';
+import { useQuery, gql } from "@apollo/client";
+
+const GET_HOUSEHOLD = gql`
+  query getUser($username: String!) {
+    queryUser (byNameFilter: $username) {
+      id
+      username
+    }
+  }
+`
 
 const App = ()  => {
+  const { loading, error, data } = useQuery(GET_HOUSEHOLD);
 
 	return (
 		<main>
+      {console.log(data)}
 			<Switch>
 				<Route exact path="/">
 					<Login />
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://411adca7-4489-40e4-9b8b-f7a45bf4102e.mock.pstmn.io/graphql/",
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 );
