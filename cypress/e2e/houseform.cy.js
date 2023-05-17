ddd7b32001d3ee30cffbe1c673aa5c99338d613a
Christopher Baum <christopherabaum@gmail.com>
2023-05-17 16:03:06 -0500

    Initialize testing for house form

---
+++ cypress/e2e/houseform.cy.js
@@ -0,0 +1,10 @@
+describe('House Form', () => {
+    beforeEach('', () => {
+      cy.visit('https://to-do-guru-ui.vercel.app/houseform');
+    });
+  
+    it('should allow a user to name their household', () => {
+      cy.get('[name="householdName"]').type('Cypress Family')
+        .should('have.value', 'Cypress Family');
+    });
+});
