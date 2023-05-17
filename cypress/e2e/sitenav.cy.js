a985877a9ff9e43c0e39eb5824ec1c0a147471b3
Christopher Baum <christopherabaum@gmail.com>
2023-05-17 14:53:58 -0500

    Finish testing for site navigation

--- cypress/e2e/sitenav.cy.js
+++ cypress/e2e/sitenav.cy.js
@@ -4,15 +4,60 @@ describe('site navigation', () => {
     });
 
     it('should allow a user to use the provided HTML buttons to navigate through the site', () => {
-        cy.get('.login-btn').click()
-        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard")
+        cy.get('.login-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+
+        cy.get('[href="/houseform"] > .nav-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/houseform");
+        cy.get('a > .house-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+
+        cy.get('[href="/choreform"] > .nav-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/choreform");
+        cy.get('.house-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+
+        cy.get('.active > .nav-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/");
     });
 
     it('should allow a user to use the browser back/forward buttons to navigate through the site', () => {
-        cy.get
+        cy.get('.login-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+
+        cy.go('back')
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/");
+
+        cy.go('forward')
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+
+        cy.get('[href="/houseform"] > .nav-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/houseform");
+        cy.go('back')
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+        cy.go('forward')
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/houseform");
+        cy.get('a > .house-btn').click();
+
+        cy.get('[href="/choreform"] > .nav-btn').click();
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/choreform");
+        cy.go('back')
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
+        cy.go('forward')
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/choreform");
     });
 
     it('should allow a user to use the address bar to navigate through the site', () => {
-        cy.get
+        cy.visit('https://to-do-guru-ui.vercel.app/');
+        cy.contains('Welcome to To-Do Guru!');
+
+        cy.visit('https://to-do-guru-ui.vercel.app/dashboard');
+        cy.contains("Turing Fam's Chore Schedule");
+
+        cy.visit('https://to-do-guru-ui.vercel.app/choreform');
+        cy.contains("Edit your Chores!");
+
+        cy.visit('https://to-do-guru-ui.vercel.app/houseform');
+        cy.contains("Edit your Household!");
     });
 });
