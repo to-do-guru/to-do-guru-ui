7e84aa9e22f5125c24d17cd603eae1c4eff5bac6
Kara Anderson <kanderson8585@gmail.com>
2023-05-18 16:15:37 -0600

    Toggle to allow household name editing and style form

--- src/Components/HouseForm/HouseForm.js
+++ src/Components/HouseForm/HouseForm.js
@@ -1,13 +1,13 @@
 import "./HouseForm.css";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_HOUSE_INFO } from "../../queries";
-import { useEffect } from "react";
 
 const HouseForm = ({ id, email }) => {
   const [members, setMembers] = useState([]);
   const [householdName, setHouseholdName] = useState("");
+  const [editMode, setEditMode] = useState(false);
 
   const { loading, data, error } = useQuery(GET_HOUSE_INFO, {
     variables: { email },
@@ -39,18 +39,32 @@ const HouseForm = ({ id, email }) => {
     // eventually delete requests to backend
   }
 
+  const toggleEdit = () => {
+    editMode ? setEditMode(false) : setEditMode(true)
+  }
+
   return (
     <div className="edit-house">
       <h1>Edit your Household!</h1>
       <form className="house-form">
-        <input
-          type="text"
-          name="householdName"
-          placeholder="Name Your Household"
-          value={householdName}
-          onChange={(e) => setHouseholdName(e.target.value)}
-          required
-        />
+        {!editMode && 
+          <div className="household-input">
+            <p>{householdName}</p>
+            <button onClick={toggleEdit}><span class="material-symbols-outlined">edit</span></button>
+          </div>}
+        {editMode &&           
+          <div className="edit-household-name">
+            <input
+              type="text"
+              name="householdName"
+              placeholder="Name Your Household"
+              value={householdName}
+              onChange={(e) => setHouseholdName(e.target.value)}
+              required
+            />
+            <button onClick={toggleEdit} className="submit-name">Submit Name</button>
+          </div>
+        }
         {memberInputs}
         {/* <button onClick={}>Add Member</button> */}
         <button className="house-btn" onClick={submitForm}>
