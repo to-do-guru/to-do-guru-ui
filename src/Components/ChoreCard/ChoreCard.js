3b417ab692488a5fa3cf94af9ebeb2f8a8d2a01e
Travis Parks <travisparks0788@gmail.com>
2023-05-19 13:06:31 -0400

    Fix accessability tabbing

--- src/Components/ChoreCard/ChoreCard.js
+++ src/Components/ChoreCard/ChoreCard.js
@@ -2,8 +2,8 @@ import './ChoreCard.css';
 
 const ChoreCard = ({member, chore, duration}) => {
   return (
-    <div className='chore-card' style={{'backgroundColor':`#${Math.floor(Math.random()*16777215).toString(16)}`}}>
-      <h3>{member}'s Chore:</h3>
+    <div className='chore-card' tabIndex="0" style={{'backgroundColor':`#${Math.floor(Math.random()*16777215).toString(16)}`}}>
+      <h1>{member}'s Chore:</h1>
       <h2>{chore}</h2>
       <p>{duration} minutes</p>
     </div>
