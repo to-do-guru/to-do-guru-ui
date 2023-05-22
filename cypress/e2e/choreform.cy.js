893c17c9f1f8a0dfc47c3233ef5ed961a0dbd311
Kara Anderson <kanderson8585@gmail.com>
2023-05-22 14:07:16 -0600

    Use conditional to stub requests

--- cypress/e2e/choreform.cy.js
+++ cypress/e2e/choreform.cy.js
@@ -1,41 +1,51 @@
+import { GET_HOUSEHOLD } from "../../src/queries";
 import { aliasOperation } from "../utils/utilityfunctions.cy";
 
 describe('Chore Form', () => {
   beforeEach('', () => {
-    cy.intercept('https://to-do-guru-api.onrender.com/graphql', (req) => {
-      aliasOperation(req, 'getHousehold', {
-        "data": {
-          "household": {
-            "chores": [{
-              "choreName": "Sweeping"
-            }],
-            "id": 1,
-            "name": "Example House",
-            "sunday": null,
-            "monday": [
-              {
-                "choreName": "Sweeping",
-                "assignedMember": "Jane Doe",
-                "duration": 30
-              }
-            ],
-            "tuesday": null,
-            "wednesday": null,
-            "thursday": null,
-            "friday": null,
-            "saturday": null
-          }
+    cy.intercept('POST', 'https://to-do-guru-api.onrender.com/graphql', (req) => {
+    if(req.body.query.includes('getHousehold')) {
+      req.reply({"body": {"data": {
+        "household": {
+          "chores": [{
+            "choreName": "Sweeping"
+          }],
+          "id": 1,
+          "name": "Example House",
+          "sunday": null,
+          "monday": [
+            {
+              "choreName": "Sweeping",
+              "assignedMember": "Jane Doe",
+              "duration": 30
+            }
+          ],
+          "tuesday": null,
+          "wednesday": null,
+          "thursday": null,
+          "friday": null,
+          "saturday": null
         }
-      });
-
-      aliasOperation(req, 'createChore', { 
-        "createChore": {
-          "chores": [{"choreName": "Mopping"}],
-          "errors": []
-        }
-      });
+      }}})
+    } else {
+      req.reply(
+        {"body": {
+          "data": {"createChore": {
+            "chores": [{"choreName": "Mopping"}],
+            "errors": []
+          }}
+        }}
+      )
+    }
+  })
+    //   aliasOperation(req, 'createChore', { 
+    //     "createChore": {
+    //       "chores": [{"choreName": "Mopping"}],
+    //       "errors": []
+    //     }
+    //   });
       
-    })
+    // })
     cy.visit('https://to-do-guru-ui.vercel.app/choreform');
   });
 
@@ -75,16 +85,15 @@ describe('Chore Form', () => {
     .get('[type="number"]').should('have.value', '240');
   });
 
-  it.only('should allow user to add chore to list', () => {
-
-    cy.get('li').should('have.length', 0);
+  it('should allow user to add chore to list', () => {
+    cy.get('li').should('have.length', 1);
 
     cy.get('[type="text"][required=""]').type('Mopping')
       .get('.css-13cymwt-control').click()
       .get('#react-select-2-option-1').click()
       .get('.chore-btn').click();
 
-    cy.get('li').should('have.length', 1);
+    cy.get('li').should('have.length', 2);
   });
 
   it('should clear the form after the user adds their chore', () => {
