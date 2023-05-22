f06c92206aed6d53155f5502144397c02e634d2b
Travis Parks <travisparks0788@gmail.com>
2023-05-22 13:35:23 -0400

    Update stubbing tests for login and dashboard

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,48 @@
+import { aliasOperation } from "../utils/utilityfunctions.cy";
+
 describe('login page', () => {
-  it.skip('should be to visit a log in page, sign in, and be taken to see the dashboard', () => {
+  beforeEach('', () => {
+    cy.intercept('https://to-do-guru-api.onrender.com/graphql', (req) => {
+      aliasOperation(req, 'getHousehold', {
+        "data": {
+          "household": {
+            "chores": [{
+              "choreName": "Sweeping"
+            }],
+            "id": 1,
+            "name": "Example House",
+            "sunday": null,
+            "monday": [
+              {
+                "choreName": "Sweeping",
+                "assignedMember": "Jane Doe",
+                "duration": 30
+              }
+            ],
+            "tuesday": null,
+            "wednesday": null,
+            "thursday": null,
+            "friday": null,
+            "saturday": null
+          }
+        }
+      });
+
+      // aliasOperation(req, 'createChore', { 
+      //   "createChore": {
+      //     "chores": [{"choreName": "Mopping"}],
+      //     "errors": []
+      //   }
+      // });
+      
+    })
+  })
+
+  it('should be to visit a log in page, sign in, and be taken to see the dashboard', () => {
     cy.visit('https://to-do-guru-ui.vercel.app/')
     cy.get('input').type("travis@gmail.com")
     cy.get('input').should("have.value", "travis@gmail.com")
     cy.get('.login-btn').click()
-    cy.url("https://to-do-guru-ui.vercel.app/dashboard")
+    cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard")
   })
 })
