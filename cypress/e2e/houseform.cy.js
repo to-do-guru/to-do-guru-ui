13451ebd2f4701f8a98960593d03bda8be3ff62e
Christopher Baum <christopherabaum@gmail.com>
2023-05-24 11:25:35 -0500

    Add graphQL stubs to houseForm test

--- cypress/e2e/houseform.cy.js
+++ cypress/e2e/houseform.cy.js
@@ -1,10 +1,52 @@
 describe('House Form', () => {
-    beforeEach('', () => {
-      cy.visit('https://to-do-guru-ui.vercel.app/houseform');
-    });
+  beforeEach('', () => {
+    cy.intercept('POST', 'https://salty-tundra-49252.herokuapp.com/graphql', (req) => {
+      if(req.body.query.includes('getHousehold')) {
+        req.reply({"body": {"data": {
+          "household": {
+            "id": 1,
+            "name": "Example House",
+            "members": [
+              {"id": "10", "name": "Beth"},
+              {"id": "11", "name": "Jerry"},
+              {"id": "12", "name": "Morty"},
+            ]
+          }
+        }}})
+      }
+      if(req.body.query.includes('updateHousehold')) {
+        req.reply({"body": {"data": {
+          "updateHousehold": {
+            "household": {
+              "name": "Cypress House"
+            }
+          }
+        }}})
+      }
+      if(req.body.query.includes('memberDelete')) {
+        req.reply({"body": {"data": {
+          "memberDelete": {
+            "member": {
+              "name": "Jerry"
+            }
+          }
+        }}})
+      }
+      if(req.body.query.includes('createMember')) {
+        req.reply({"body": {"data": {
+          "createMember": {
+            "member": {
+              "id": "509",
+              "name": "Rick"
+            }
+          }
+        }}})
+      }
+    })
+ 
+    cy.visit('http://localhost:3000/houseform');
+      // cy.visit('https://to-do-guru-ui.vercel.app/choreform');
+  });
   
-    it.skip('should allow a user to name their household', () => {
-      cy.get('[name="householdName"]').type('Cypress Family')
-        .should('have.value', 'Cypress Family');
-    });
+
 });
