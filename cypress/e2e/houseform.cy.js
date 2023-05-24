262fde8279c515b8da7fd1f11ff10542a2400d3a
Christopher Baum <christopherabaum@gmail.com>
2023-05-24 11:48:06 -0500

    Add happy path testing for houseform

--- cypress/e2e/houseform.cy.js
+++ cypress/e2e/houseform.cy.js
@@ -7,8 +7,8 @@ describe('House Form', () => {
             "id": 1,
             "name": "Example House",
             "members": [
-              {"id": "10", "name": "Beth"},
-              {"id": "11", "name": "Jerry"},
+              {"id": "10", "name": "Jerry"},
+              {"id": "11", "name": "Beth"},
               {"id": "12", "name": "Morty"},
             ]
           }
@@ -48,5 +48,38 @@ describe('House Form', () => {
       // cy.visit('https://to-do-guru-ui.vercel.app/choreform');
   });
   
+  it('should allow a user to re-name their household', () => {
+    cy.get('.household-input')
+      .get('h2').contains('Example House');
 
+    cy.get('.material-symbols-outlined').click();
+    cy.get('#householdName').clear().type('Cypress Family');
+    cy.get('.submit-name').click();
+
+    cy.get('.household-input')
+      .get('h2').contains('Cypress House');
+  });
+
+  it('should allow a user to add a member to their household', () => {
+    cy.get('p').should('have.length', 3);
+    cy.get('p').last().contains('Morty');
+
+    cy.get('.house-form > .house-btn').click();
+    cy.get('#memberInput').type("Rick");
+    cy.get('.submit-member').click();
+
+    cy.get('p').should('have.length', 4);
+    cy.get('p').last().contains('Rick');
+  });
+
+  it('should allow a user to delete a member of their household', () => {
+    cy.get('p').should('have.length', 3);
+    cy.get('p').first().contains('Jerry');
+
+    cy.get('.house-form > :nth-child(2)').contains('Jerry');
+    cy.get(':nth-child(2) > .delete').click();
+
+    cy.get('p').should('have.length', 2);
+    cy.get('p').first().contains('Beth');
+  });
 });
