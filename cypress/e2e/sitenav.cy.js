e634f9c7884ab591dad6e07ca658d0192811d976
Christopher Baum <christopherabaum@gmail.com>
2023-05-24 12:21:28 -0500

    Update site nav cypress test and add test for redirecting user

--- cypress/e2e/sitenav.cy.js
+++ cypress/e2e/sitenav.cy.js
@@ -1,9 +1,41 @@
 describe('site navigation', () => {
     beforeEach(() => {
+        cy.intercept('POST', 'https://salty-tundra-49252.herokuapp.com/graphql', (req) => {
+            if(req.body.query.includes('getHousehold')) {
+                req.reply({"body": {"data": {
+                    "household": {
+                    "members": [
+                        {"id": "10", "name": "Jerry"},
+                        {"id": "12", "name": "Morty"},
+                        ],
+                    "chores": [{
+                        "choreName": "Sweeping"
+                    }],
+                    "id": 1,
+                    "name": "Example House",
+                    "sunday": null,
+                    "monday": [
+                        {
+                        "choreName": "Sweeping",
+                        "assignedMember": "Jane Doe",
+                        "duration": 30
+                        }
+                    ],
+                    "tuesday": null,
+                    "wednesday": null,
+                    "thursday": null,
+                    "friday": null,
+                    "saturday": null
+                    }
+                }}})
+            }
+        })
+
         cy.visit('https://to-do-guru-ui.vercel.app/');
     });
 
-    it.skip('should allow a user to use the provided HTML buttons to navigate through the site', () => {
+    it('should allow a user to use the provided HTML buttons to navigate through the site', () => {
+        cy.get('input').type('smith@example.com');
         cy.get('.login-btn').click();
         cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
 
@@ -21,7 +53,8 @@ describe('site navigation', () => {
         cy.url().should('eq', "https://to-do-guru-ui.vercel.app/");
     });
 
-    it.skip('should allow a user to use the browser back/forward buttons to navigate through the site', () => {
+    it('should allow a user to use the browser back/forward buttons to navigate through the site', () => {
+        cy.get('input').type('smith@example.com');
         cy.get('.login-btn').click();
         cy.url().should('eq', "https://to-do-guru-ui.vercel.app/dashboard");
 
@@ -47,17 +80,26 @@ describe('site navigation', () => {
         cy.url().should('eq', "https://to-do-guru-ui.vercel.app/choreform");
     });
 
-    it.skip('should allow a user to use the address bar to navigate through the site', () => {
+    it('should allow a user to use the address bar to navigate through the site', () => {
         cy.visit('https://to-do-guru-ui.vercel.app/');
         cy.contains('Welcome to To-Do Guru!');
 
         cy.visit('https://to-do-guru-ui.vercel.app/dashboard');
-        cy.contains("Turing Fam's Chore Schedule");
+        cy.contains("Example House Chore Schedule");
 
         cy.visit('https://to-do-guru-ui.vercel.app/choreform');
         cy.contains("Edit your Chores!");
 
         cy.visit('https://to-do-guru-ui.vercel.app/houseform');
         cy.contains("Edit your Household!");
+    });
+
+    it("should redirect a user if they attempt to navigate to a URL that doesn't exist", () => {
+        cy.visit('https://to-do-guru-ui.vercel.app/');
+        cy.contains('Welcome to To-Do Guru!');
+
+        cy.visit('https://to-do-guru-ui.vercel.app/randombaloney/nonsense');
+        cy.url().should('eq', "https://to-do-guru-ui.vercel.app/");
+        cy.contains("Welcome to To-Do Guru!");
     });
 });
